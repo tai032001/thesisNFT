613411dd63e98432a2379005bc543ee5a5feaf9c
Tai Quach <quachhungtai29@gmail.com>
2023-08-15 10:16:14 +0700

    update css files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { Sepolia } from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
+      activeChain={Sepolia}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
