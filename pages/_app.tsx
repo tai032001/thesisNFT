32874445c47177a9827259bde26ecd673c5f6516
Tai Quach <quachhungtai29@gmail.com>
2023-09-12 14:02:14 +0700

    Config head title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import { Sepolia } from "@thirdweb-dev/chains";
+import Head from "next/head";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -13,6 +14,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={Sepolia}
     >
+      <Head>
+        <link rel="icon" href="/images/monster.ico" />
+        <title>ThesisGame</title>
+      </Head>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
